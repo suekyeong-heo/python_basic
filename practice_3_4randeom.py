d5e5ea18998c9f5c5926219099a1570c70984e3d
sue <sue123131@gmail.com>
2025-05-25 14:46:20 +0900

    quiz3

--- practice_3_4randeom.py
+++ practice_3_4randeom.py
@@ -15,3 +15,12 @@ print(int(random()*10+1)) #1~10이하의 임의의 값 생성
 print(int(random()*10+1)) #1~10이하의 임의의 값 생성
 
 print(int(random()*45+1)) #1~45이하의 임의의 값 생성
+print(randrange(1,46)) #1~46미만의 임의의 값 생성
+print(randrange(1,46)) #1~46미만의 임의의 값 생성
+print(randrange(1,46)) #1~46미만의 임의의 값 생성
+print(randrange(1,46)) #1~46미만의 임의의 값 생성
+
+print(randint(1,45)) #1~45 이하의 임의의 값 생성
+print(randint(1,45)) #1~45 이하의 임의의 값 생성
+print(randint(1,45)) #1~45 이하의 임의의 값 생성
+print(randint(1,45)) #1~45 이하의 임의의 값 생성
